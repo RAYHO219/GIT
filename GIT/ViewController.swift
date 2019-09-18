74b499de568441928fac447ce80980fd279a2743
RAY <raywnwa@gmail.com>
2019-09-18 22:27:09 +0800

    add message string reverse

--- GIT/ViewController.swift
+++ GIT/ViewController.swift
@@ -9,9 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let message = "Hello Git"
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(message)
+        func reverse(text: String)->String{
+            return String(text.reversed())
+            
+        }
+        print(reverse(text: "stressed"))
         // Do any additional setup after loading the view.
     }
 
